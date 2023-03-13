e22f56f7702b7537b3bd030f3ac1315fec47cc6c
Emile Verebi <emile.verebi@outlook.fr>
2023-03-13 16:28:56 +0100

    created model

---
+++ src/main/java/com/bigcorp/booking/model/Fournisseurs.java
@@ -0,0 +1,61 @@
+package com.bigcorp.booking.model;
+
+import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Fournisseurs")
+public class Fournisseurs {
+	@Id
+	private Integer iD;
+	private Integer numeroUnique;
+	private String nom;
+	private String mail;
+	private String adresse;
+
+	// Getters
+	public Integer getiD() {
+		return this.iD;
+	}
+
+	public Integer getNumeroUnique() {
+		return this.numeroUnique;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public String getMail() {
+		return this.mail;
+	}
+
+	public String getAdresse() {
+		return this.adresse;
+	}
+
+	// Setters
+
+	public void setiD(Integer iD) {
+		this.iD = iD;
+	}
+
+	public void setNumeroUnique(Integer numeroUnique) {
+		this.numeroUnique = numeroUnique;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+}
