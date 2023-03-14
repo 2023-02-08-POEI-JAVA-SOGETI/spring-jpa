f6b8b616171afac3135bfff20712ab8c573f0a23
Silvio <silvio.rea07@gmail.com>
2023-03-14 09:11:06 +0100

    add fournisseur model class

--- src/main/java/com/bigcorp/booking/exercice/Fournisseur.java
+++ src/main/java/com/bigcorp/booking/exercice/Fournisseur.java
@@ -1,5 +1,78 @@
 package com.bigcorp.booking.exercice;
 
-public class TEST_FOR_COMMINT {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="fournisseur")
+public class Fournisseur {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Integer id;
+	
+	
+	private String nom;
+	private String num;
+	private String email;
+	private String adresse;
+	
+	
+	
+	public Fournisseur() {
+		super();
+	}
+	
+	
+	
+	public Fournisseur(String nom, String num, String email, String adresse) {
+		super();
+		this.nom = nom;
+		this.num = num;
+		this.email = email;
+		this.adresse = adresse;
+	}
+
+
+
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
+	
+	
+	
+	
 
 }
