f427b99a06c49372f3f17f73f63ecdeb83a280fe
Emile Verebi <emile.verebi@outlook.fr>
2023-03-17 10:59:36 +0100

    Spring marche vraiment pas

---
+++ src/main/java/com/bigcorp/booking/service/MonDao.java
@@ -0,0 +1,12 @@
+package com.bigcorp.booking.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MonDao {
+
+	public void salut() {
+		System.out.println("salut ça fonctionne");
+	}
+
+}
--- src/main/java/com/bigcorp/booking/spring/SpringApplication.java
+++ src/main/java/com/bigcorp/booking/spring/SpringApplication.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.bigcorp.booking.model.Example;
 import com.bigcorp.booking.service.ExampleService;
+import com.bigcorp.booking.service.MonControleurWeb;
 
 
 public class SpringApplication {
 
 	public static void main(String[] args) {
 		try (AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
-			ExampleService exampleService = appContext.getBean("exampleService", ExampleService.class);
-			exampleService.save(new Example());
+			//ExampleService exampleService = appContext.getBean("exampleService", ExampleService.class);
+			//exampleService.save(new Example());
+			MonControleurWeb monControleurWeb = appContext.getBean(MonControleurWeb.class);
+			monControleurWeb.getMonService().getMonDao().salut();
 		}
 	}
 
