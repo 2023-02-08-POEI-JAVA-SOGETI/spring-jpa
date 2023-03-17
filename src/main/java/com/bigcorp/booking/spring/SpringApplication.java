094ca5ce9c3cd60aea48f827606537f4608abc0b
François Caremoli <francois.caremoli@wanadoo.fr>
2023-03-17 10:31:42 +0100

    Modification de SpringApplication pour le TP

--- src/main/java/com/bigcorp/booking/spring/SpringApplication.java
+++ src/main/java/com/bigcorp/booking/spring/SpringApplication.java
@@ -2,16 +2,16 @@ package com.bigcorp.booking.spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.bigcorp.booking.model.Example;
-import com.bigcorp.booking.service.ExampleService;
+import com.bigcorp.booking.service.ClasseA;
 
 
 public class SpringApplication {
 
 	public static void main(String[] args) {
-		try (AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
-			ExampleService exampleService = appContext.getBean("exampleService", ExampleService.class);
-			exampleService.save(new Example());
+		try (AnnotationConfigApplicationContext appContext 
+				= new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
+			ClasseA beanClasseA = appContext.getBean(ClasseA.class);
+			beanClasseA.getClasseB().getClasseC().affiche();
 		}
 	}
 
