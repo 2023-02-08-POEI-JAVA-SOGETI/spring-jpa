94a82a3ee35a634c6fa31b811f07f37471da96a4
lucas.vittaz <lucasvittaz.pro@gmail.com>
2023-03-17 11:05:34 +0100

    tp : spring annotations @Componant et @Autowired

---
+++ src/main/java/com/bigcorp/booking/service/MonDao.java
@@ -0,0 +1,14 @@
+package com.bigcorp.booking.service;
+
+import javax.persistence.EntityManager;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MonDao {
+
+	public void test() {
+		System.out.println("ça fonctionne 👌👌👌👌👌👌👌" );
+	}
+}
--- src/main/java/com/bigcorp/booking/spring/SpringApplication.java
+++ src/main/java/com/bigcorp/booking/spring/SpringApplication.java
@@ -4,15 +4,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.bigcorp.booking.model.Example;
 import com.bigcorp.booking.service.ExampleService;
+import com.bigcorp.booking.service.MonControleurWeb;
 
 
 public class SpringApplication {
 
 	public static void main(String[] args) {
 		try (AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
-			ExampleService exampleService = appContext.getBean("exampleService", ExampleService.class);
-			exampleService.save(new Example());
-		}
+			
+//			ExampleService exampleService = appContext.getBean("exampleService", ExampleService.class);
+//			exampleService.save(new Example());
+			MonControleurWeb monControleurWeb = appContext.getBean("monControleurWeb", MonControleurWeb.class);
+			monControleurWeb.getMonService().getMonDao().test();	
+			
+			}
 	}
 
 }
