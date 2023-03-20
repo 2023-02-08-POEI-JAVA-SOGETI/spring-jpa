14214eb23141bc192ae8be31e1b02ce28a2cb3a2
François Caremoli <francois.caremoli@wanadoo.fr>
2023-03-20 08:54:21 +0100

    Correction logback pour Spring MVC

---
+++ src/main/java/com/bigcorp/booking/service/MonControleurWeb.java
@@ -0,0 +1,27 @@
+package com.bigcorp.booking.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+
+public class MonControleurWeb {
+
+	@Autowired
+	private MonService monService;
+	
+
+	
+
+	public MonService getMonService() {
+		return monService;
+	}
+
+	public void setMonService(MonService monService) {
+		this.monService = monService;
+	}	
+	
+
+}
--- src/main/java/com/bigcorp/booking/spring/SpringApplication.java
+++ src/main/java/com/bigcorp/booking/spring/SpringApplication.java
@@ -1,18 +1,31 @@
 package com.bigcorp.booking.spring;
 
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.bigcorp.booking.service.simple.ClasseA;
+import com.bigcorp.booking.service.MonControleurWeb;
 
 
 public class SpringApplication {
 
 	public static void main(String[] args) {
-		try (AnnotationConfigApplicationContext appContext 
-				= new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
-			ClasseA beanClasseA = appContext.getBean(ClasseA.class);
-			beanClasseA.getClasseB().getClasseC().affiche();
+//		try (AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
+//			ExampleService exampleService = appContext.getBean("exampleService", ExampleService.class);
+//			exampleService.save(new Example());
+//		}
+		
+		//Récupérer une instance de MonControleurWeb 
+	
+		try (AnnotationConfigApplicationContext appContext = 
+				new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
+			MonControleurWeb monControleurWeb = appContext.getBean(MonControleurWeb.class);	
+			
+			System.out.println(monControleurWeb.getMonService().getMonDao());
+			monControleurWeb.getMonService().getMonDao().save(null);
+			
 		}
+	
+	
 	}
 
 }
