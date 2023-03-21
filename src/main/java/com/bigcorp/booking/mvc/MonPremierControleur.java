515b198594b434c76eb80ce316aa944969b89660
François Caremoli <francois.caremoli@wanadoo.fr>
2023-03-21 15:31:58 +0100

    Dynamisation de vue-salut

--- src/main/java/com/bigcorp/booking/mvc/MonPremierControleur.java
+++ src/main/java/com/bigcorp/booking/mvc/MonPremierControleur.java
@@ -1,27 +1,31 @@
 package com.bigcorp.booking.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Annotée par @Controller, cette 
- * classe va être considérée par Spring
- * MVC pour traiter des requêtes.
+ * Annotée par @Controller, cette classe va être considérée par Spring MVC pour
+ * traiter des requêtes.
  */
 @Controller
 public class MonPremierControleur {
-	
-    
-    /**
-     * Intercepte les requêtes /salut , effectue
-     * des instructions (ici un simple System.out) puis
-     * renvoie vers la vue salut.
-     * @return
-     */
-    @RequestMapping("/salut")
-    public String saveSayHello() {
-    	System.out.println("Le contrôleur de salut agit ! ");
-        return "vue-salut";
-    }
-    
+
+	/**
+	 * Intercepte les requêtes /salut , effectue des instructions (ici un simple
+	 * System.out) puis renvoie vers la vue salut.
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/salut")
+	public String saveSayHello() {
+		System.out.println("Le contrôleur de salut agit ! ");
+		return "vue-salut";
+	}
+
+	@ModelAttribute("vraiTexte")
+	public String getVraiTexte() {
+		return "Ceci est le vrai texte !";
+	}
+
 }
