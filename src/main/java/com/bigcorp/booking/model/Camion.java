e35ababb6d06acc652b724684708ea3cd545421f
lucas.vittaz <lucasvittaz.pro@gmail.com>
2023-03-22 09:04:28 +0100

    tp thymeleaf

---
+++ src/main/java/com/bigcorp/booking/model/Camion.java
@@ -0,0 +1,14 @@
+package com.bigcorp.booking.model;
+
+public class Camion {
+	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+}
---
+++ src/main/java/com/bigcorp/booking/mvc/CoucouController.java
@@ -0,0 +1,44 @@
+package com.bigcorp.booking.mvc;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bigcorp.booking.model.Camion;
+
+@Controller
+public class CoucouController {
+	 
+	@RequestMapping("/coucou")
+	public String showSayHello() {
+	    	System.out.println("Le contrôleur de coucou agit ! ");
+	        return "vue-coucou";
+	    }
+	@ModelAttribute("helloFrom")
+	public String helloFrom() {
+		 return "From Coucou controller at :" +" "+ java.time.LocalDate.now();
+	}
+	
+	@ModelAttribute("chauffeMarcel")
+	public Camion chauffeMarcel() {
+		Camion marcel = new Camion();
+		marcel.setNom("Chauffe Marcel !!!");
+		 return marcel;
+	}
+	
+	@ModelAttribute("listCamion")
+	public List<Camion> getCamionList() {
+		Camion man = new Camion();
+		Camion renault = new Camion();
+		renault.setNom("renault 300 ");
+		man.setNom("man");
+		List<Camion> camions = new ArrayList<>();
+		camions.add(renault);
+		camions.add(man);
+		return camions;
+		}
+}
