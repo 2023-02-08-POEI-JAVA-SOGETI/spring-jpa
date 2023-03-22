45bb2d661d8bfec8653be17b17ae4d28b2938fbc
Emile Verebi <emile.verebi@outlook.fr>
2023-03-22 15:37:15 +0100

    created fournisseurs page and displayed manually created fournisseurs on page

---
+++ src/main/java/com/bigcorp/booking/mvc/ControleurFournisseurs.java
@@ -0,0 +1,40 @@
+package com.bigcorp.booking.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bigcorp.booking.service.spring.FournisseurSpringService;
+import com.bigcorp.booking.model.Fournisseurs;
+
+@Controller
+public class ControleurFournisseurs {
+	@Autowired
+	private FournisseurSpringService fournisseurService;
+
+	@RequestMapping("/fournisseurs")
+	private ModelAndView showPageFournisseur() {
+		System.out.println("Le contrôleur de fournisseurs agit ! ");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("vue-fournisseurs");
+		Fournisseurs fourn1 = new Fournisseurs();
+		Fournisseurs fourn2 = new Fournisseurs();
+		Fournisseurs fourn3 = new Fournisseurs();
+		fourn3.setNom("Emile");
+		fourn2.setNom("Emule");
+		fourn1.setNom("Emole");
+		List<Fournisseurs> fournisseursList = new ArrayList<Fournisseurs>();
+		fournisseursList.add(fourn1);
+		fournisseursList.add(fourn2);
+		fournisseursList.add(fourn3);
+		mav.addObject("fournisseursModel", fournisseursList);
+		System.out.println("Rendu terminé (normalement hein)");
+		return mav;
+		
+	}
+}
