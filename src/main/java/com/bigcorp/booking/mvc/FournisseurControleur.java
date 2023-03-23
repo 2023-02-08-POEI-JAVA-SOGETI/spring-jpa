80a2cf0f49be0aae39b4b81861ce1504c6563379
Pablo_Rial <pablorialgil@outlook.es>
2023-03-23 10:12:39 +0100

    Update methode @PostMapping, binding and redirect added

--- src/main/java/com/bigcorp/booking/mvc/FournisseurControleur.java
+++ src/main/java/com/bigcorp/booking/mvc/FournisseurControleur.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bigcorp.booking.service.FournisseurService;
 
@@ -25,9 +27,12 @@ public class FournisseurControleur {
     }
 	
 	@PostMapping("/fournisseur")
-	public String saveFournisseur(@ModelAttribute("fournisseur") Fournisseur fournisseur) {
+	public ModelAndView saveFournisseur(@ModelAttribute("fournisseur") Fournisseur fournisseur,
+			BindingResult result) {
 	    fournisseurService.save(fournisseur);
-	    return "formulaire-fournisseur";
+	    ModelAndView mav = new ModelAndView();
+    	mav.setViewName("redirect:/formulaire-fournisseur?id=" + fournisseur.getId());
+	    return mav;
 	}
 	
 	@Autowired
