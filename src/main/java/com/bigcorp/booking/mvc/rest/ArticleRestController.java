8336a812a11edc6984f41344095613e2016830eb
Emile Verebi <emile.verebi@outlook.fr>
2023-03-24 16:25:12 +0100

    added delete in ArticleRestController

--- src/main/java/com/bigcorp/booking/mvc/rest/ArticleRestController.java
+++ src/main/java/com/bigcorp/booking/mvc/rest/ArticleRestController.java
@@ -2,8 +2,10 @@ package com.bigcorp.booking.mvc.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -40,5 +42,24 @@ public class ArticleRestController {
 		article.setNom("Ceci est un test");
 		return article;
 	}
+	
+	@DeleteMapping("/articles/{articleId}")
+	public void deleteById(@PathVariable("articleId") Integer articleId) {
+		Article article = articleService.findById(articleId);
+		if (article == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aucun article trouvé avec l'id : " + articleId + ", pas de suppression effectuée");
+		}
+		articleService.delete(articleId);
+		System.out.println("J'ai supprimé l'article " + articleId);
+	}
+	
+	/*@PostMapping("/articles}")
+	public ArticleRestDto getById(@PathVariable("articleId") Integer articleId) {
+		Article article = articleService.findById(articleId);
+		if (article == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aucun article trouvé avec l'id : " + articleId);
+		}
+		return new ArticleRestDto(article);
+	}*/
 
 }
