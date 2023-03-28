1f88eca9b92141d00e0bf12e4d306d1394d65b4f
Maxime Fontenelle <fontenelle.maxime@gmail.com>
2023-03-28 14:49:06 +0200

    debug de la tuyauterie

---
+++ src/main/java/com/restaurant/dao/RestaurantDao.java
@@ -0,0 +1,11 @@
+package com.restaurant.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.restaurant.model.Restaurant;
+
+@Repository
+public interface RestaurantDao extends CrudRepository<Restaurant, Integer> {
+
+}
--- src/main/java/com/restaurant/model/Reservation.java
+++ src/main/java/com/restaurant/model/Reservation.java
@@ -1,7 +1,5 @@
 package com.restaurant.model;
 
-import com.restaurant.model.restaurant;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
