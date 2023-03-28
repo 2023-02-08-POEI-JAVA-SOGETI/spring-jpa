f95560202db91f7887a940c9d2a8242fb5dbfc52
Manon Bourgognon <manon.bourgognon@gmail.com>
2023-03-28 10:54:00 +0100

    add info TP perso

--- src/main/java/com/bigcorp/booking/service/ArticleService.java
+++ src/main/java/com/bigcorp/booking/service/ArticleService.java
@@ -66,8 +66,58 @@ public class ArticleService {
 		return this.articleSpringDao.findAll();
 	}
 	
+	
+	/**
+	 * Récupère des articles selon leur état.
+	 * La liste des articles est triée par nom.
+	 * @param etat
+	 * @return la liste des articles triée par nom
+	 */
+//	public List<Article> findByEtat(EtatArticle etat) {
+//		LOGGER.info("Récupération des articles avec l'état: {}" , etat);
+//		return this.articleSpringDao.findByEtatArticleOrderByNom(etat);
+//	}
+	
+
+	//doit être persisté en base. Si l'identifiant d'article est null : 
+	//une exception est lancée. Si l'identifiant du fournisseur est null,
+	//l'article n'est plus rattaché à aucun fournisseur (si votre BDD le permet). 
+	//La méthode renvoie l'article modifié en base. La méthode doit être 
+	//testée avec un test JUnit.
+
+	/**
+	 * Attache l'article avec l'id articleId au fournisseur avec
+	 * l'id fournisseurId. idARticle est non null et correspond à 
+	 * un Article en base,sinon, une Exception est lancée
+	 * @param articleId
+	 * @param fournisseurId
+	 * @return
+	 * @throws IllegalArgumentException si articleId est null
+	 * @throws NoSuchElementException si articleId ne correspond à rien en base de données
+	 */
+//	public Article attache(Integer articleId, Integer fournisseurId) {
+//		LOGGER.info("Rattachement de l'article : {} avec le fournisseur : {} " , articleId, fournisseurId);
+//		if(articleId == null) {
+//			throw new IllegalArgumentException("articleId ne peut être null");
+//		}
+//		Optional<Article> optionalArticle = this.articleSpringDao.findById(articleId);
+//		Article article = optionalArticle.orElse(null);
+//		
+//		Fournisseur fournisseur = null;
+//		if(fournisseurId != null) {
+//			fournisseur = fournisseurSpringDao.findById(fournisseurId).orElse(null);			
+//		}
+//		article.setFournisseur(fournisseur);
+//		
+//		return this.articleSpringDao.save(article);
+//		
+//	}
+	
+	
 	//METHODE BONUS A COMPLETERE
 //	public Article persistArticleWithFournisseur(Integer idArticle, Integer idFournisseur) {	
 //		return Article ;
 //	}
+	
+	
 }
