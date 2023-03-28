a38468619634ff5d10bd1ed6356176532c0de9a1
Emile Verebi <emile.verebi@outlook.fr>
2023-03-28 15:03:19 +0200

    added data script

--- sql/Creation_Tables.sql
+++ sql/Creation_Tables.sql
@@ -28,3 +28,8 @@ CREATE TABLE type_restaurant
 
 ALTER TABLE restaurant ADD FOREIGN KEY (type_restaurant_id) REFERENCES type_restaurant(id);
 ALTER TABLE reservation ADD FOREIGN KEY (restaurant_id) REFERENCES restaurant(id);
+
+INSERT INTO restaurant (nom, prix, adresse) VALUES ('Chez Pierre', 'TRES_BON_MARCHE', '12 rue de la Paix');
+INSERT INTO restaurant (nom, prix, adresse) VALUES ('Le Bistrot', 'BON_MARCHE', '8 avenue des Champs-Élysées');
+INSERT INTO restaurant (nom, prix, adresse) VALUES ('Le Ritz', 'CHER', '15 place Vendôme');
+INSERT INTO restaurant (nom, prix, adresse) VALUES ('La Tour d\'Argent', 'TRES_CHER', '17 Quai de la Tournelle');
